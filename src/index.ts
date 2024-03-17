eecb4f6af26391799f3716c32fdcd441cc4c84ec
sumitk@deqode.com <sumitk@deqode.com>
2024-03-18 01:12:15 +0530

    worked on restaurant moddal and controller

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,32 @@
-import express, {Request, Response} from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
-import myUserRoute  from "./routes/MyUserRoute"
+import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
-    .then(() => console.log("connected to Database")) 
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to Database"));
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.get("/health", async (req:Request, res:Response) => {
-      res.send({message:" health OK!"})
-})
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: " health OK!" });
+});
 
-
-app.use("/api/my/user", myUserRoute)
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 app.listen(7000, () => {
-     console.log("server started on 70000")
-})
+  console.log("server started on 70000");
+});
--- src/middleware/validation.ts
+++ src/middleware/validation.ts
@@ -6,13 +6,12 @@ const handleValidationErorrs = async (
   res: Response,
   next: NextFunction
 ) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-       return res.status(400).json({ error: errors.array() });   
-    }
-    
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ error: errors.array() });
+  }
+
   next();
-  
 };
 
 export const validateMyUserRequest = [
@@ -23,5 +22,29 @@ export const validateMyUserRequest = [
     .withMessage("Address must be string"),
   body("city").isString().notEmpty().withMessage("City must be string"),
   body("country").isString().notEmpty().withMessage("Country must be string"),
- handleValidationErorrs
+  handleValidationErorrs,
+];
+
+export const validateMyRestaurantRequest = [
+  body("restaurantName").notEmpty().withMessage("Restaurant name is required"),
+  body("city").notEmpty().withMessage("City is required"),
+  body("country").notEmpty().withMessage("country is required"),
+  body("deliveryPrice")
+    .notEmpty()
+    .withMessage("deliveryPrice must be postive number"),
+  body("deliveryTime")
+    .isInt({ min: 0 })
+    .withMessage("Estimated delivery time must be a postive integer"),
+  body("cuisines")
+    .isArray()
+    .withMessage("Cuisines must be array")
+    .not()
+    .isEmpty()
+    .withMessage("Cuisins array cannot be empty"),
+  body("menuItems").isArray().withMessage("Menu items must be array"),
+  body("menuItems.*.name").notEmpty().withMessage("Menu item name is required"),
+  body("menuItems.*.price")
+    .isFloat({ min: 0 })
+    .withMessage("Menu item price is required and must be a positive number"),
+  handleValidationErorrs,
 ];
